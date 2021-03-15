8ecdda7b7985fca07389f9af23fc749c6106ceeb
japankid-code <japankid.jake@gmail.com>
2021-03-14 22:01:15 -0500

    button click now adds list item dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 buttonEl.addEventListener("click", function() {
-    alert("button clicked");
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
 });
