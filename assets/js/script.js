d80fd127f1043fbf07e9a825b081358beaf7c3b2
japankid-code <japankid.jake@gmail.com>
2021-03-15 18:34:41 -0500

    move task creation to new function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    };
+
+var createTaskEl = function(taskDataObj) {
     // create the elements and add classes
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -12,8 +16,8 @@ var createTaskHandler = function(event) {
     taskInfoEl.className = "task-info";
     // add HTML content to div
     taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
-    tasksToDoEl.appendChild(listItemEl); // adds the task list item to the list
     listItemEl.appendChild(taskInfoEl); // puts a div (adding content) inside the task list item
-    };
+    tasksToDoEl.appendChild(listItemEl); // adds the entire task list item to the list
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
